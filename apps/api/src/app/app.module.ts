e0e7708b31c769229b86d1a1a9fd57f988fbf404
Sergio Reyes <sergio.reyes@plexusworldwide.com>
2024-07-06 12:10:18 -0600

    Starting

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import path, { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'web/.next'),
+      exclude: ['/api/*']
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
